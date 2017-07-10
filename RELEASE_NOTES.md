85fc649f2e9d2e600099a7c0f9e114ddd1c77f74
ncthbrt <nick@cuthbert.co.za>
2017-07-10 21:12:39 +0200

    Initial package publish

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+## 0.1.0
+- Intial release of library
+
--- build.fsx
+++ build.fsx
@@ -12,15 +12,21 @@ open Fake.YarnHelper
 let buildDir  = "./build/"
 let deployDir = "./deploy/"
 
+let nugetProjectFolders = [ (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./src"))  
+                            (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./src"))
+                          ]
+                          |> Array.concat
+                          |> Seq.map (fun m -> m.Directory.FullName)
 
-
-let projectFolders  =  [ (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./src"))  
+let projectFolders  =  [
+                         (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./src"))  
                          (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./src"))
                          (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./samples/"))
                          (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./samples/"))
-                       ] 
+                       ]                       
                        |> Array.concat
                        |> Seq.map (fun m -> m.Directory.FullName)
+
 
 
 // Targets
@@ -55,9 +61,32 @@ Target "RunElmishSample" (fun _ ->
     ] |> Async.Parallel |> Async.RunSynchronously |> ignore
 )
 
+let release =  ReadFile "RELEASE_NOTES.md" |> ReleaseNotesHelper.parseReleaseNotes                
+
+Target "Meta" (fun _ ->
+    [ "<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">"
+      "<PropertyGroup>"
+      "<Description>Library for strongly typed websocket use in Fable</Description>"
+      "<PackageProjectUrl>https://github.com/ncthbrt/Fable.Websockets.Elmish</PackageProjectUrl>"
+      "<PackageLicenseUrl>https://github.com/ncthbrt/Fable.Websockets/blob/master/LICENSE.md</PackageLicenseUrl>"
+      "<PackageIconUrl></PackageIconUrl>"
+      "<RepositoryUrl>https://github.com/ncthbrt/Fable.Websockets.Elmish</RepositoryUrl>"
+      "<PackageTags>fable;fsharp;elmish;websockets;observables</PackageTags>"
+      "<Authors>Nick Cuthbert</Authors>"
+      sprintf "<Version>%s</Version>" (string release.SemVer)
+      "</PropertyGroup>"
+      "</Project>"]
+    |> WriteToFile false "src/Meta.props"    
+)
+
+
+Target "Package" (fun _ ->            
+    !! @"./src/**/*.fsproj"
+    |> Seq.iter (fun project-> DotNetCli.Pack (fun p-> { p with Project = project; OutputPath = currentDirectory+"/build" }))
+)
 
 // Build order
-"Clean" ==> "Restore" ==> "YarnRestore" ==> "Build"
+"Meta" ==> "Clean" ==> "Restore" ==> "YarnRestore" ==> "Build" ==> "Package"
 
 "Build" ==> "RunElmishSample"
 
